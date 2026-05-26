531fc9bb9d970ac45d3837d78403eda97c0abda4
Aurélie DELVAR <adelvar@norsys.fr>
2026-05-26 16:18:39 +0200

    fix: add user information

---
+++ api/src/main/java/com/neighborrhub/api/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.neighborrhub.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.neighborrhub.api.dto.UserDto;
+import com.neighborrhub.api.entity.User;
+
+@Controller
+@RequestMapping("/api/users")
+public class UserController {
+    
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> getCurrentUser(@AuthenticationPrincipal User currentUser) {
+        return ResponseEntity.ok(toDto(currentUser));
+    }
+
+    private UserDto toDto(User user) {
+        UserDto dto = new UserDto();
+        dto.setId(user.getId());
+        dto.setMail(user.getMail());
+        dto.setName(user.getName());
+        return dto;
+    }
+}
---
+++ api/src/main/java/com/neighborrhub/api/dto/UserDto.java
@@ -0,0 +1,10 @@
+package com.neighborrhub.api.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private Long id;
+    private String name;
+    private String mail;
+}
